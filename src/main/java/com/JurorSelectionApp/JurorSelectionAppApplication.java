11cbe595731274c6fbc3894006ee229fbde907b6
BlaineHalvorson <blainehalvorson29@gmail.com>
2021-05-18 13:51:42 -0400

    youtube fix attempt

--- src/main/java/com/JurorSelectionApp/JurorSelectionAppApplication.java
+++ src/main/java/com/JurorSelectionApp/JurorSelectionAppApplication.java
@@ -2,12 +2,18 @@ package com.JurorSelectionApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JurorSelectionAppApplication {
+public class JurorSelectionAppApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(JurorSelectionAppApplication.class, args);
+	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(JurorSelectionAppApplication.class);
 	}
 
 }

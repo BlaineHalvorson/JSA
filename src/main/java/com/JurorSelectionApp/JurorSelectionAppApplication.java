8442b71d5429bdeb792b1817b8d5cf43bec6e75e
BlaineHalvorson <blainehalvorson29@gmail.com>
2021-05-18 13:56:42 -0400

    revert

--- src/main/java/com/JurorSelectionApp/JurorSelectionAppApplication.java
+++ src/main/java/com/JurorSelectionApp/JurorSelectionAppApplication.java
@@ -2,18 +2,12 @@ package com.JurorSelectionApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JurorSelectionAppApplication extends SpringBootServletInitializer{
+public class JurorSelectionAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JurorSelectionAppApplication.class, args);
-	}
-
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
-		return application.sources(JurorSelectionAppApplication.class);
 	}
 
 }
